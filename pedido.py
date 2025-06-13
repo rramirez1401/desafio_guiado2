69e28b06182840f366daf44cd8a2b2024a2a1037
rramirez1401 <raul.ramirez1401+1@gmail.com>
2025-06-13 00:59:53 +0000

    Desafío finalizado

--- pedido.py
+++ pedido.py
@@ -1,7 +1,9 @@
 from te import Te
 
-Te.limpiar_pantalla()
+Te.limpiar_pantalla()  ## Limpiar pantalla
 
+
+## solicito el saor del té con un input
 sabor = int(input("""
 Selecciona el sabor del té:
 1 : Té Negro
@@ -10,16 +12,23 @@ Selecciona el sabor del té:
 
 >>> """))
 
-Te.limpiar_pantalla()
+Te.limpiar_pantalla()  ## Limpiar pantalla
 
+
+## solicito el formato del té
 formato = int(input("Elige el formato: 300 ó 500 gramos:\n\n>>> "))
 
+
+## LLamamos al método estático "receta" de la clase "Te" para obtener el nombre, el tiempo y la recomendación del té
 nombre, tiempo, recomendacion = Te.receta(sabor)
 
+## LLamamos al método estático "obtener_precio" de la clase "Te" para obtener el precio según el formato
 precio = Te.obtener_precio(formato)
 
-Te.limpiar_pantalla()
+Te.limpiar_pantalla()  ## Limpiar pantalla
 
+
+## Imprimo en pantalla el resultado del pedido
 print(f"""
 Tu elección es la siguiente      
     
